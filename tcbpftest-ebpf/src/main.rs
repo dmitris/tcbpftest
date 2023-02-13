f3cc39131dc9f231392ce972296defec3ad824f2
Dmitry Savintsev <dmitris@users.noreply.github.com>
2023-02-13 21:12:39 +0100

    sync with aya-template (#16)

--- tcbpftest-ebpf/src/main.rs
+++ tcbpftest-ebpf/src/main.rs
@@ -1,14 +1,14 @@
 #![no_std]
 #![no_main]
 
+use aya_bpf::bindings::__sk_buff;
+use aya_bpf::helpers::bpf_skb_pull_data;
 use aya_bpf::{
-    BpfContext,
     macros::{classifier, map},
     maps::PerfEventArray,
     programs::TcContext,
+    BpfContext,
 };
-use aya_bpf::bindings:: __sk_buff;
-use aya_bpf::helpers::bpf_skb_pull_data;
 use core::mem;
 use memoffset::offset_of;
 
@@ -27,9 +27,10 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 }
 
 #[map(name = "EVENTS")]
-static mut EVENTS: PerfEventArray<PacketLog> = PerfEventArray::<PacketLog>::with_max_entries(1024, 0);
+static mut EVENTS: PerfEventArray<PacketLog> =
+    PerfEventArray::<PacketLog>::with_max_entries(1024, 0);
 
-#[classifier(name="tcbpftest")]
+#[classifier(name = "tcbpftest")]
 pub fn tcbpftest(ctx: TcContext) -> i32 {
     match unsafe { try_tcbpftest(ctx) } {
         Ok(ret) => ret,
@@ -40,13 +41,13 @@ pub fn tcbpftest(ctx: TcContext) -> i32 {
 const ETH_P_IP: u16 = 0x0800;
 const ETH_HDR_LEN: usize = mem::size_of::<ethhdr>();
 const IP_HDR_LEN: usize = mem::size_of::<iphdr>();
-const IPPROTO_TCP : u8  = 6;
-const IPPROTO_UDP : u8 = 17;
+const IPPROTO_TCP: u8 = 6;
+const IPPROTO_UDP: u8 = 17;
 
 unsafe fn try_tcbpftest(ctx: TcContext) -> Result<i32, i64> {
     let ctx_len = ctx.len();
     let skb = ctx.as_ptr() as *mut __sk_buff;
-    if  bpf_skb_pull_data(skb, ctx_len) != 0 {
+    if bpf_skb_pull_data(skb, ctx_len) != 0 {
         return Err(199);
     }
     // get the ethernet header proto field as well as the IP protocol one
@@ -63,37 +64,37 @@ unsafe fn try_tcbpftest(ctx: TcContext) -> Result<i32, i64> {
     let rem_port2 = u16::from_be(ctx.load(ETH_HDR_LEN + IP_HDR_LEN + offset_of!(tcphdr, source))?);
     let loc_port2 = u16::from_be(ctx.load(ETH_HDR_LEN + IP_HDR_LEN + offset_of!(tcphdr, dest))?);
 
-    let rem_port_val : u16;
-    let loc_port_val : u16;
+    let rem_port_val: u16;
+    let loc_port_val: u16;
     unsafe {
-      rem_port_val = match ptr_at(&ctx, ETH_HDR_LEN + IP_HDR_LEN + offset_of!(tcphdr, source)) {
+        rem_port_val = match ptr_at(&ctx, ETH_HDR_LEN + IP_HDR_LEN + offset_of!(tcphdr, source)) {
             Err(_) => return Err(197),
             Ok(val) => *val,
-      };
-      loc_port_val =  match  ptr_at(&ctx, ETH_HDR_LEN + IP_HDR_LEN + offset_of!(tcphdr, dest)) {
-                Err(_) => return Err(198),
-                Ok(val) => *val,
-            };
+        };
+        loc_port_val = match ptr_at(&ctx, ETH_HDR_LEN + IP_HDR_LEN + offset_of!(tcphdr, dest)) {
+            Err(_) => return Err(198),
+            Ok(val) => *val,
+        };
     }
     let rem_port = u16::from_be(rem_port_val);
     let loc_port = u16::from_be(loc_port_val);
 
-    let mut udp_len_val : u16 = 0;
+    let mut udp_len_val: u16 = 0;
     if ip_proto == IPPROTO_UDP {
-      unsafe {
-        udp_len_val = match ptr_at(&ctx, ETH_HDR_LEN + IP_HDR_LEN + offset_of!(udphdr, len)) {
-            Err(_) => return Err(197),
-            Ok(val) => *val,
-        };
-      }
+        unsafe {
+            udp_len_val = match ptr_at(&ctx, ETH_HDR_LEN + IP_HDR_LEN + offset_of!(udphdr, len)) {
+                Err(_) => return Err(197),
+                Ok(val) => *val,
+            };
+        }
     }
     let log_entry = PacketLog {
         len: length as u32,
         ctx_len: ctx_len,
         src_addr: saddr,
         dest_addr: daddr,
-	    eth_proto: eth_proto as u32,
-	    eth_proto2: eth_proto2 as u32,
+        eth_proto: eth_proto as u32,
+        eth_proto2: eth_proto2 as u32,
         ip_proto: ip_proto as u32,
         remote_port: rem_port as u32,
         remote_port2: rem_port2 as u32,
@@ -112,12 +113,12 @@ unsafe fn try_tcbpftest(ctx: TcContext) -> Result<i32, i64> {
 //
 #[inline(always)]
 unsafe fn ptr_at<T>(ctx: &TcContext, offset: usize) -> Result<*const T, ()> {
-    let raw_skb  = ctx.as_ptr() as *const __sk_buff;
+    let raw_skb = ctx.as_ptr() as *const __sk_buff;
     let start = (*raw_skb).data as usize;
     let end = (*raw_skb).data_end as usize;
     let len = mem::size_of::<T>();
 
-    if start + offset  + len  > end {
+    if start + offset + len > end {
         return Err(());
     }
 
